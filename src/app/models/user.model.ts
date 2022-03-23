ad17bc013309d963bf881e2e4be61bc8e72bb6ab
duongtanhuan <duongtanhuan2000@gmail.com>
2022-03-23 23:28:11 +0700

    add-user

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,15 @@
+export class User   { 
+ 
+    constructor(id: number, firstName: string , lastName: string, email: string) {
+        this.id=id;
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.email=email;
+    }
+ 
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+ 
+}
